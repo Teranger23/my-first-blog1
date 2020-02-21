79518a28f71cfa7bfd09d278de7619e27ccd39de
Gerardo Lopez Teran <teran_gerardo23@hotmail.com>
2020-02-21 12:46:05 -0600

    Fix:author

--- blog/views.py
+++ blog/views.py
@@ -12,7 +12,6 @@ from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
-from django.contrib.auth.models import User
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -26,8 +25,8 @@ def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
-            post = form.save(commit=False)           
-            post.author = User.objects.get(username='glopezt')
+            post = form.save(commit=False)
+            post.author = request.user
             post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
@@ -41,8 +40,8 @@ def post_edit(request, pk):
     if request.method == "POST":
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
-            post = form.save(commit=False)        
-            post.author = User.objects.get(username='glopezt')
+            post = form.save(commit=False)
+            post.author = request.user
             post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
